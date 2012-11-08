3c065f7281f5ab133382ce8f8bdb1cd550aed50a
Jairo Vazquez <jairo.vazquez@agilekoding.com>
2012-11-08 17:58:18 -0600

    Added more tmpl custom formats

--- vendor/assets/javascripts/lib/jquery.formatting.tmpl.js
+++ vendor/assets/javascripts/lib/jquery.formatting.tmpl.js
@@ -1,39 +1,108 @@
-function tmplDatetimeParse(datetime){
-  if (datetime == null) return "";
+(function(){
 
-  if (datetime.split("T").length === 1)
-    datetime = datetime + "T00:00:00"
+  window.TmplCustomFormats = {}
+  var parseDateString;
 
-  var date   = datetime.split("T")[0];
-  var d      = date.split("-");
+  $.extend(jQuery.tmpl.tag, {
+      'format_currency': {
+          open: '_.push($.formatNumber(parseFloat($1), {format:"$#,###.00", locale:"us"}));'
+      },
 
-  var time   = datetime.split("T")[1].split("-")[0];
-  var t      = time.split(":");
+      'format_currency_without_space': {
+          open: '_.push($.formatNumber(parseFloat($1), {format:"$#,###.00", locale:"us"}));'
+      },
 
-  var myDate = new Date(d[0], (parseInt(d[1], 10) - 1), d[2], t[0], t[1], t[2]);
+      'format_currency_without_decimals': {
+        open: '_.push($.formatNumber(parseFloat($1), {format:"$ #,###", locale:"us"}));'
+      },
 
-  return $.datepicker.formatDate("dd-M-yy", myDate)
-}
+      'format_number': {
+          open: '_.push($.formatNumber(parseInt($1), {format:"#,###", locale:"us"}));'
+      },
 
-$.extend(jQuery.tmpl.tag, {
-    'format_currency': {
-        open: '_.push($.formatNumber(parseFloat($1), {format:"$ #,###.00", locale:"us"}));'
-    },
+      'format_date': {
+          open: '_.push(TmplCustomFormats.format_date($1));'
+      },
 
-    'format_currency_without_space': {
-        open: '_.push($.formatNumber(parseFloat($1), {format:"$#,###.00", locale:"us"}));'
-    },
+      'format_datetime': {
+          open: '_.push(TmplCustomFormats.format_datetime($1));'
+      },
 
-    'format_currency_without_decimals': {
-      open: '_.push($.formatNumber(parseFloat($1), {format:"$ #,###", locale:"us"}));'
-    },
+      't': {
+          open: '_.push(TmplCustomFormats.t($1));'
+      },
 
-    'format_number': {
-        open: '_.push($.formatNumber(parseInt($1), {format:"#,###", locale:"us"}));'
-    },
+      'format_boolean': {
+          open: '_.push(TmplCustomFormats.format_boolean($1));'
+      }
 
-    'format_date': {
-        open: '_.push(tmplDatetimeParse($1));'
-    }
+  });
 
-});
+  // TmplCustomFormats Format Functions
+  // =================================================================
+
+  TmplCustomFormats.format_date = function(datetime){
+    if (datetime == null) return "";
+
+    if (datetime.split(/[T|t]/).length === 1)
+      datetime = datetime + "T00:00:00"
+
+    var date   = datetime.split(/[T|t]/)[0];
+    var d      = parseDateString(date).split(/-|\//);
+
+    var time   = datetime.split(/[T|t]/)[1].split("-")[0];
+    var t      = time.split(":");
+
+    var myDate = new Date(d[0], (parseInt(d[1], 10) - 1), d[2], t[0], t[1], t[2]);
+
+    return $.datepicker.formatDate("dd-M-yy", myDate)
+  }
+
+  TmplCustomFormats.format_datetime = function(datetime){
+    if (datetime == null) return "";
+
+    if (datetime.split(/[T|t]/).length === 1)
+      datetime = datetime + "T00:00:00"
+
+    var date = TmplCustomFormats.format_date(datetime);
+    var time = datetime.split(/[T|t]/)[1].split("-")[0];
+
+    return date + " " + time
+  }
+
+  TmplCustomFormats.t = function(value){
+    if (_.isArray(value)) value = value.join(".");
+    return Modules.I18n.t(value);
+  }
+
+  TmplCustomFormats.format_boolean = function(value){
+    value = value == true;
+    return Modules.I18n.t("helpers.booleans." + value);
+  }
+
+
+  // TmplCustomFormats Format Helpers
+  // =================================================================
+
+  parseDateString = function(date){
+    // yyyy/mm/dd
+    if (/^(19|20)\d\d[- \/.](0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])$/.test(date)) return date;
+
+    var dateArray = date.split(/-|\//);
+
+    if (/^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)\d\d$/.test(date)){
+      date = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+    } // dd/mm/yyyy
+
+    else if (/^(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d$/.test(date)){
+      date = dateArray[2] + "/" + dateArray[0] + "/" + dateArray[1];
+    } // mm/dd/yyyy
+
+    else if (/^(19|20)\d\d[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])$/.test(date)){
+      date = dateArray[0] + "/" + dateArray[2] + "/" + dateArray[1];
+    } // yyyy/dd/mm
+
+    return date; // return format: yyyy/mm/dd
+  }
+
+}).call(this);
